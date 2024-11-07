56eee4138c9bcf9d1097047ddadf7c809f3aa8fe
rogerioboldrin <rogerioboldrin@gmail.com>
2024-11-07 17:04:04 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppGorjeta/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiAppGorjeta
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        } 
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double valorConta = Convert.ToDouble(entValorConta.Text);
+            double perGorjeta = Convert.ToDouble(entPerGorjeta.Text);
+            double valorGorjeta = valorConta * perGorjeta/100;
+            double total = valorGorjeta + valorConta;
+
+            lbValorGorjeta.Text = "R$" + valorConta.ToString("F2");
+            lbValorTotal.Text = "R$" + total.ToString("F2");
+
+
+
+        }
+    }
+
+}
